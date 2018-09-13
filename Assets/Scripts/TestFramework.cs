c3a0f1c4b6afc425e8be7243e2629195f01b846d
袁剑伟 <251410454@qq.com>
2018-09-13 13:37:33 +0800

    添加链表类

---
+++ Assets/Scripts/TestFramework.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFramework : MonoBehaviour
+{
+    private NodeList<string, int> nodeList;
+
+    // Use this for initialization
+    void Start()
+    {
+        nodeList = new NodeList<string, int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            nodeList.ClearNodes();
+            nodeList.AddNode("message", 1);
+            nodeList.AddNode("message", 2);
+            nodeList.AddNode("message", 3);
+            nodeList.AddNode("message", 4);
+            nodeList.AddNode("message", 5);
+            nodeList.ShowNodes("message");
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            nodeList.RemoveNode("message", 1);
+            nodeList.ShowNodes("message");
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            nodeList.RemoveNode("message", 3);
+            nodeList.ShowNodes("message");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            nodeList.RemoveNode("message", 5);
+            nodeList.ShowNodes("message");
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            nodeList.ClearNodes();
+            nodeList.AddNode(new string[2] { "message1", "message2" }, 1);
+            nodeList.AddNode("message1", 2);
+            nodeList.AddNode(new string[2] { "message1", "message2" }, 3);
+            nodeList.AddNode("message2", 4);
+            nodeList.AddNode(new string[2] { "message1", "message2" }, 5);
+            nodeList.ShowNodes("message1");
+            nodeList.ShowNodes("message2");
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            nodeList.RemoveNode("message1", 1);
+            nodeList.ShowNodes("message1");
+            nodeList.ShowNodes("message2");
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            nodeList.RemoveNode("message2", 5);
+            nodeList.ShowNodes("message1");
+            nodeList.ShowNodes("message2");
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/NodeList.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeList<TKey, TValue>
+{
+    private Dictionary<TKey, Node<TValue>> nodeDic;
+
+    public NodeList()
+    {
+        nodeDic = new Dictionary<TKey, Node<TValue>>();
+    }
+
+    /// <summary>
+    /// 添加节点
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public void AddNode(TKey key, TValue value)
+    {
+        if (!nodeDic.ContainsKey(key))
+        {
+            nodeDic.Add(key, new Node<TValue>(value));
+        }
+        else
+        {
+            Node<TValue> tempNode = nodeDic[key];
+            while (tempNode.next != null)
+            {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = new Node<TValue>(value);
+        }
+    }
+
+    /// <summary>
+    /// 添加节点
+    /// </summary>
+    /// <param name="keys"></param>
+    /// <param name="value"></param>
+    public void AddNode(TKey[] keys, TValue value)
+    {
+        foreach (var key in keys)
+        {
+            AddNode(key, value);
+        }
+    }
+
+    /// <summary>
+    /// 删除节点
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public void RemoveNode(TKey key, TValue value)
+    {
+        if (!nodeDic.ContainsKey(key))
+        {
+            throw new Exception("Key not exist!");
+        }
+        else
+        {
+            //value位于头部
+            if (nodeDic[key].data.Equals(value))
+            {
+                //只有一个节点，直接删除key
+                if (nodeDic[key].next == null)
+                {
+                    nodeDic.Remove(key);
+                }
+                //有下个节点，将头节点指向下个节点
+                else
+                {
+                    nodeDic[key] = nodeDic[key].next;
+                }
+            }
+            //value位于中间、尾部
+            else
+            {
+                Node<TValue> tempNode = nodeDic[key];
+                while (tempNode.next != null && !tempNode.next.data.Equals(value))
+                {
+                    tempNode = tempNode.next;
+                }
+                //value位于中间，将节点的下个节点指向下个节点
+                if (tempNode.next.next != null)
+                {
+                    tempNode.next = tempNode.next.next;
+                }
+                //value位于尾部，将节点的下个节点置空
+                else
+                {
+                    tempNode.next = null;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 删除节点
+    /// </summary>
+    /// <param name="keys"></param>
+    /// <param name="value"></param>
+    public void RemoveNode(TKey[] keys, TValue value)
+    {
+        foreach (var key in keys)
+        {
+            RemoveNode(key, value);
+        }
+    }
+
+    /// <summary>
+    /// 清除所有节点
+    /// </summary>
+    public void ClearNodes()
+    {
+        nodeDic.Clear();
+    }
+
+    /// <summary>
+    /// 显示节点
+    /// </summary>
+    /// <param name="key"></param>
+    public void ShowNodes(TKey key)
+    {
+        if (!nodeDic.ContainsKey(key))
+        {
+            throw new Exception("Key not exist!");
+        }
+        else
+        {
+            Node<TValue> tempNode = nodeDic[key];
+            Debug.Log(key + ": " + tempNode.data);
+            while (tempNode.next != null)
+            {
+                tempNode = tempNode.next;
+                Debug.Log(key + ": " + tempNode.data);
+            }
+        }
+    }
+}
+
+public class Node<T>
+{
+    public T data;
+    public Node<T> next;
+
+    public Node(T data)
+    {
+        this.data = data;
+    }
+}
